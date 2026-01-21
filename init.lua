9b1898b7d74b10a1bd7a5e83208fb537a068b721
iwarszawski <igor.warszawski@rtbhouse.com>
2026-01-21 18:52:54 +0100

    new helm parsers, monokai pro

--- init.lua
+++ init.lua
@@ -737,6 +737,15 @@ require('lazy').setup({
               },
             },
           },
+          helm_ls = {
+            settings = {
+              ['helm-ls'] = {
+                yamlls = {
+                  path = 'yaml-language-server',
+                },
+              },
+            },
+          },
           -- rust_analyzer = {},
           -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
           --
@@ -967,11 +976,11 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'loctvl842/monokai-pro.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
+      require('monokai-pro').setup {
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
@@ -980,7 +989,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'monokai-pro-ristretto'
     end,
   },
 
@@ -1043,6 +1052,10 @@ require('lazy').setup({
         'query',
         'vim',
         'vimdoc',
+        --
+        'yaml',
+        'helm',
+        'python',
       }
 
       -- Install above parsers if they are missing.
@@ -1078,6 +1091,17 @@ require('lazy').setup({
   -- place them in the correct locations.
 
   -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
+
+  {
+    'qvalentin/helm-ls.nvim',
+    ft = 'helm',
+    opts = {
+      conceal_templates = {
+        enabled = false, -- this might change to false in the future
+      },
+    },
+  },
+
   --
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
